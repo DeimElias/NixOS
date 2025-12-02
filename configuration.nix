caf1ea5764ee5d8a226ab5df00b38790cd9d3f0f
DeimElias <deimelias@gmail.com>
2025-12-02 02:28:08 -0600

    Desktop: Add mpv and remove Bluetooth configuration

--- configuration.nix
+++ configuration.nix
@@ -134,6 +134,7 @@
       neovim
       kdePackages.okular
       usbutils
+      mpv
       inputs.rose-pine-hyprcursor.packages.${pkgs.system}.default
       #  wget
     ]
@@ -192,35 +193,6 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     jack.enable = true;
-    wireplumber.extraConfig = {
-      "50-bluez" = {
-        "monitor.bluez.properties" = {
-          "bluez5.roles" = [
-            "a2dp_sink"
-            "a2dp_source"
-            "bap_sink"
-            "bap_source"
-          ];
-
-          "bluez5.codecs" = [
-            "ldac"
-            "aptx"
-            "aptx_ll_duplex"
-            "aptx_ll"
-            "aptx_hd"
-            "opus_05_pro"
-            "opus_05_71"
-            "opus_05_51"
-            "opus_05"
-            "opus_05_duplex"
-            "aac"
-            "sbc_xq"
-          ];
-
-          "bluez5.hfphsp-backend" = "none";
-        };
-      };
-    };
   };
 
   security.polkit.enable = true;
